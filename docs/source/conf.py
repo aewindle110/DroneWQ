f1902a358e731627abf3e65c1507a65dc330b1b4
Patrick Gray <pgrayobx@gmail.com>
2025-01-07 19:55:26 -0500

    testing config for autogen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,16 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../')) # Source code dir relative to this file
+
+extensions = [
+    'sphinx.ext.autodoc',  # Core library for html generation from docstrings
+    'sphinx.ext.autosummary',  # Create neat summary tables
+]
+
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +33,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
