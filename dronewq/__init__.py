ab8fb9a7e707857051df37297390cfc9efad238a
kkk23 <kurtis.kwan@dukekunshan.edu.cn>
2025-10-02 22:53:40 -0400

    spelling error in init.py

--- dronewq/__init__.py
+++ dronewq/__init__.py
@@ -2,7 +2,7 @@ from dronewq.utils.settings import settings
 from dronewq.utils.metadata import write_metadata_csv
 from dronewq.utils.images import retrieve_imgs_and_metadata
 from dronewq.lw_methods import *
-import dronewq.lecacy
+import dronewq.legacy
 
 # Singleton definitions and aliasing
 configure = settings.configure
