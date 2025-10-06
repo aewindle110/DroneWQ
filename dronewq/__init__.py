d4fc1a2ca571553932cee7f103f9e7c3a8648653
Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
2025-10-05 23:09:22 -0400

    Refactor downsample, georeferencing. Closes #17 #18

--- dronewq/__init__.py
+++ dronewq/__init__.py
@@ -1,12 +1,21 @@
-from dronewq.utils.settings import settings
-from dronewq.utils.metadata import write_metadata_csv
-from dronewq.utils.images import retrieve_imgs_and_metadata, process_micasense_images
-from dronewq.lw_methods import *
-from dronewq.ed_methods import *
-from dronewq.masks import *
-from dronewq.core.raw_to_rss import process_raw_to_rrs
-from dronewq.core.wq_calc import save_wq_imgs
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __init__.py
+
+from .utils.settings import settings
+from .utils.metadata import write_metadata_csv
+from .utils.images import retrieve_imgs_and_metadata, process_micasense_images
+from .lw_methods import *
+from .ed_methods import *
+from .masks import *
+from .core.raw_to_rss import process_raw_to_rrs
+from .core.wq_calc import save_wq_imgs
+from .core.downsample import downsample
+from .core.georeference import compute_flight_lines, georeference
 import dronewq.legacy
 
 # Singleton definitions and aliasing
 configure = settings.configure
+
+
+__version__ = "1.0"
