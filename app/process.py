d239e33a620148393b2d7ced07bc9ed57f3f32a7
Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
2025-11-29 22:13:19 -0500

    takes in new vmin and vmax for wq algs. Closes #114

--- app/process.py
+++ app/process.py
@@ -1,6 +1,6 @@
 # Everything related to computing and plotting results
 # from description_generator import generate_plot_descriptions
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models.model_project import Project
 from pipeline import Pipeline
 
@@ -65,4 +65,15 @@ def process_updated(project_id: int):
 #     args = request.get_json(silent=True) or request.args
 
 
-# @bp.route("/api/plot/wq", methods=["POST"])
+@bp.route("/api/plot/wq", methods=["POST"])
+def update_wq_plot():
+    args = request.get_json(silent=True) or request.args
+    project_id = args.get("projectId")
+    plot_args = args.get("plotParams")
+    try:
+        settings = Project.get_project(project_id)
+        pipeline = Pipeline(settings.to_dict())
+        pipeline.plot_wq(plot_args)
+        return jsonify({"Success": True}), 200
+    except LookupError as e:
+        return jsonify({str(e): f"Project {project_id} not found"})
